ef8b3ce780ef2b5729c43211d574b7eb7102728b
Gregor Labadan <gregorlorenzolabadan@gmail.com>
2026-02-23 21:49:49 +0800

    fix(library): reduce content padding on mobile

--- src/pages/SessionLibraryPage.tsx
+++ src/pages/SessionLibraryPage.tsx
@@ -48,7 +48,7 @@ export default function SessionLibraryPage() {
       </div>
 
       {/* Content */}
-      <div className="max-w-5xl mx-auto p-6 space-y-4">
+      <div className="max-w-5xl mx-auto p-4 sm:p-6 space-y-4">
         {/* Search + filter row */}
         <div className="flex gap-3">
           <Input
